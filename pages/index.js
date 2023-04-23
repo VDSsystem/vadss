b805624016965b13ded084df0fae8b185cc46573
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-24 01:14:37 +0300

    a

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { InferGetServerSidePropsType } from 'next'
+
+
+export default function Home({
+}) {
+  return (
+    <div className="container">
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
