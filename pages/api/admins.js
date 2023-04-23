3eadde5f77c07978e457b30a29cddd33e237bbc6
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-24 01:58:59 +0300

    newest

--- pages/api/admins.js
+++ pages/api/admins.js
@@ -1,6 +1,6 @@
 import { connectToDatabase } from "../../lib/mongodb"
 export default async function handler(req, res){
     const db = await connectToDatabase()
-    const data = await db.collection("admins").find()
+    const data = await db.collection("admins").find().toArray()
     res.json(data)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import { InferGetServerSidePropsType } from 'next'
-
 
 export default function Home({
 }) {
