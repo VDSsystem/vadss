6734359ed3533914dc16cf0abcadd60052360b10
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-24 03:32:35 +0300

    client

--- pages/api/admins.js
+++ pages/api/admins.js
@@ -1,6 +1,8 @@
 import { connectToDatabase } from "../../lib/mongodb"
 export default async function handler(req, res){
-    const db = await connectToDatabase()
-    const data = await db.collection("admins").find().json()
+    const { mongoClient } = await connectToDatabase()
+    const db = mongoClient.db("test")
+    const collection = db.collection("admins")
+    const data = await collection.find().toArray()
     res.json(data)
 }
