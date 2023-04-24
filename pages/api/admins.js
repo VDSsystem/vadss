dec4b45fd919bf1464cc0cec6743a852c7d283a4
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-24 03:56:32 +0300

    tmaaam

--- pages/api/admins.js
+++ pages/api/admins.js
@@ -1,8 +1,20 @@
 import { connectToDatabase } from "../../lib/mongodb"
-export default async function handler(req, res){
+
+export default async function handler(req, res) {
     const { mongoClient } = await connectToDatabase()
     const db = mongoClient.db("test")
     const collection = db.collection("admins")
-    const data = await collection.find().toArray()
-    res.json(data)
-}
+    switch (req.method) {
+      case "POST":
+        let newAdmin = JSON.parse(JSON.stringify(req.body));
+        console.log(req.body)
+        const addedAdmin = await collection.insertOne(newAdmin);
+        res.json(addedAdmin);
+        break;
+      case "GET":
+        const admins = await collection.find({}).toArray();
+        res.json({ status: 200, data: admins });
+        break;
+    }
+  }
+  
