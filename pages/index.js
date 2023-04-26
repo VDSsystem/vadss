0bc012b72b28a2045b9222ce87bcab846d4cdb5b
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-04-26 13:20:45 +0300

    newestANDfinal

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const MyPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/main.html');
+  }, []);
+
   return (
     <div>
       <h1>Hello World!</h1>
