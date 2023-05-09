4c8b71b0595ac0f24018a32ba4c61ff675064e8f
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-10 00:32:22 +0300

    output

---
+++ pages/api/output.js
@@ -0,0 +1,40 @@
+import { connectToDatabase } from "../../lib/mongodb"
+
+export default async function handler(req, res) {
+  const { mongoClient } = await connectToDatabase()
+  const db = mongoClient.db("test")
+  const collection = db.collection("output")
+  switch (req.method) {
+    case "POST":
+  const newImageString = req.body;
+  const newImage = JSON.parse(newImageString);
+  let maxId = 0;
+  const result = await collection.aggregate([
+    { $sort: { id: -1 } },
+    { $limit: 1 }]).toArray();
+  if (result.length > 0) {
+    maxId = result[0].id;
+  } //finding the maximumID 
+  const newId = maxId + 1;
+  newImage.id = newId;
+  const addedImage = await collection.insertOne(newImage);
+  console.log(addedImage);
+  if (!addedImage) {
+    res.status(401).json({ success: false });
+  } else {
+    res.status(200).json({id: newId});
+  }
+  break;
+  case "GET":
+    const id = req.query.id;
+    const image = await collection.findOne({ id: parseInt(id) });
+    if (!image) {
+      res.status(404).json({ message: "Image not found" });
+    } else {
+      const url = image.url;
+      res.status(200).json({url });
+    }
+    break;
+  
+  }
+}
