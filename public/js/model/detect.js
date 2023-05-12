4757c93dc8811af0843d6d9d5d8cf0da8093fad8
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 00:59:29 +0300

    newest

--- public/js/model/detect.js
+++ public/js/model/detect.js
@@ -2,46 +2,7 @@ import uploadFiles from "../controller/uploadHandler.js";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl";
 const Acc_MODEL = [{ name: "yolov5", child: ["best_web_model"] }];
-
-/*const imageRef = document.querySelector("#image");
-const videoRef = document.querySelector("#video");
-const canvasRef = document.querySelector("#canvas");
-const inputImageRef = document.querySelector("#inputImage");
-*/
-//let model = null;
-let aniId = null;
 let modelName = Acc_MODEL[0];
-let loading = 0;
-/*
-let singleImage = false;
-let liveWebcam = false;
-
-function setModel(mod) {
-  model = mod;
-}
-
-function setLoading(fractions) {
-  loading = fractions;
-}
-
-async function loadModel() {
-  await tf.loadGraphModel(`/model/${modelName.name}/${modelName.child[0]}/model.json`, {
-    onProgress: setLoading,
-  }).then(async (mod) => {
-    // warming up the model before using real data
-    const dummy = tf.ones(mod.inputs[0].shape);
-    const res = await mod.executeAsync(dummy);
-
-    // clear memory
-    tf.dispose(res);
-    tf.dispose(dummy);
-
-    // save to state
-    setModel(mod);
-  }).catch((err) => {
-    console.error("Error loading model:", err);
-  });
-}*/
 
 async function loadModel() {
   const model = await tf.loadGraphModel(`/model/${modelName.name}/${modelName.child[0]}/model.json`);
