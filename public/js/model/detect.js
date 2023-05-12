3eccc06c2dc3aea5f4e853a7b9834f8827ac7fdd
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 00:47:57 +0300

    n

--- public/js/model/detect.js
+++ public/js/model/detect.js
@@ -1,5 +1,52 @@
 import uploadFiles from "../controller/uploadHandler.js";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl";
+const Acc_MODEL = [{ name: "yolov5", child: ["best_web_model"] }];
+
+const imageRef = document.querySelector("#image");
+const videoRef = document.querySelector("#video");
+const canvasRef = document.querySelector("#canvas");
+const inputImageRef = document.querySelector("#inputImage");
+
+let model = null;
+let aniId = null;
+let modelName = Acc_MODEL[0];
+let loading = 0;
+
+let singleImage = false;
+let liveWebcam = false;
+
+function setModel(mod) {
+  model = mod;
+}
+
+function setLoading(fractions) {
+  loading = fractions;
+}
+
+function loadModel() {
+  tf.loadGraphModel(`/model/${modelName.name}/${modelName.child[0]}/model.json`, {
+    onProgress: setLoading,
+  }).then(async (mod) => {
+    // warming up the model before using real data
+    const dummy = tf.ones(mod.inputs[0].shape);
+    const res = await mod.executeAsync(dummy);
+
+    // clear memory
+    tf.dispose(res);
+    tf.dispose(dummy);
+
+    // save to state
+    setModel(mod);
+  }).catch((err) => {
+    console.error("Error loading model:", err);
+  });
+}
+
+
 window.onload = async () => {
+  loadModel();
+
     const uploadBTN = document.querySelector('.upload');
     uploadBTN.addEventListener('click', handleDetect);
 }
@@ -19,12 +66,4 @@ async function handleDetect(){
   const form = document.querySelector("form")
   form.reset()
 
-}
-function toResult(image, type){
-    return `
-    <section class="One">
-    <img src="${image}" alt="">
-    <p>Acciedent ${type}</p>
-    </section>
-    `;
 }
