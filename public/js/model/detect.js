83b8807d6c939e7447b7a446b96debd7dc096821
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 03:27:34 +0300

    old

--- public/js/model/detect.js
+++ public/js/model/detect.js
@@ -1,22 +1,5 @@
 import uploadFiles from "../controller/uploadHandler.js";
-import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs-backend-webgl";
-const Acc_MODEL = [{ name: "yolov5", child: ["best_web_model"] }];
-let modelName = Acc_MODEL[0];
-
-async function loadModel() {
-  const model = await tf.loadGraphModel(`/model/${modelName.name}/${modelName.child[0]}/model.json`);
-  console.log('Model loaded successfully!');
-  // Use the model for inference, evaluation, or fine-tuning
-}
-
-loadModel().catch(error => {
-  console.log('Error loading model: ', error);
-});
-
 window.onload = async () => {
-  loadModel();
-
     const uploadBTN = document.querySelector('.upload');
     uploadBTN.addEventListener('click', handleDetect);
 }
@@ -36,4 +19,12 @@ async function handleDetect(){
   const form = document.querySelector("form")
   form.reset()
 
+}
+function toResult(image, type){
+    return `
+    <section class="One">
+    <img src="${image}" alt="">
+    <p>Acciedent ${type}</p>
+    </section>
+    `;
 }
