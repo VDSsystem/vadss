8eccd74fdbbc24e6d464086d96b49bf0e775955e
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 08:14:26 +0300

    sss

--- pages/api/output.js
+++ pages/api/output.js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
   if (!addedImage) {
     res.status(401).json({ success: false });
   } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json({id: newId});
   }
   break;
