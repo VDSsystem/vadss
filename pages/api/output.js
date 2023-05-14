3fa9ebdb51be1cd75365989053f8453ea9f140b9
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-14 18:26:12 +0300

    ssssssssss

--- pages/api/output.js
+++ pages/api/output.js
@@ -10,20 +10,26 @@ export default async function handler(req, res) {
   let maxId = 0;
   const result = await collection.aggregate([
     { $sort: { id: -1 } },
-    { $limit: 1 }]).toArray();
+    { $limit: 1 }
+  ]).toArray();
+  
   if (result.length > 0) {
     maxId = result[0].id;
   } //finding the maximumID 
+  
   const newId = maxId + 1;
   newImageString.id = newId;
   const addedImage = await collection.insertOne(newImageString);
+  
   console.log(addedImage);
+  
   if (!addedImage) {
     res.status(401).json({ success: false });
   } else {
-    res.status(200).json({id: newId});
+    res.status(200).json({ id: newId });
   }
   break;
+
   case "GET":
     const id = req.query.id;
     const image = await collection.findOne({ id: parseInt(id) });
