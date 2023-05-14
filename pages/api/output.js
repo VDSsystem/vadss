5d0b3944e522780450fa86a14f2c002c7f135628
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-14 17:43:21 +0300

    sssssssssss

--- pages/api/output.js
+++ pages/api/output.js
@@ -6,24 +6,24 @@ export default async function handler(req, res) {
   const collection = db.collection("output")
   switch (req.method) {
     case "POST":
-  const newImageString = req.body.url;
-  let maxId = 0;
-  const result = await collection.aggregate([
-    { $sort: { id: -1 } },
-    { $limit: 1 }]).toArray();
-  if (result.length > 0) {
-    maxId = result[0].id;
-  } //finding the maximumID 
-  const newId = maxId + 1;
-  newImageString.id = newId;
-  const addedImage = await collection.insertOne(newImageString);
-  console.log(addedImage);
-  if (!addedImage) {
-    res.status(401).json({ success: false });
-  } else {
-    res.status(200).json({id: newId});
-  }
-  break;
+      const newImageString = { url: req.body.url };
+      let maxId = 0;
+      const result = await collection.aggregate([
+        { $sort: { id: -1 } },
+        { $limit: 1 }]).toArray();
+      if (result.length > 0) {
+        maxId = result[0].id;
+      } //finding the maximumID 
+      const newId = maxId + 1;
+      newImageString.id = newId;
+      const addedImage = await collection.insertOne(newImageString);
+      console.log(addedImage);
+      if (!addedImage) {
+        res.status(401).json({ success: false });
+      } else {
+        res.status(200).json({id: newId});
+      }
+      break;
   case "GET":
     const id = req.query.id;
     const image = await collection.findOne({ id: parseInt(id) });
