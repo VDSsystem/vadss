71215e31e5a89ad95ba0e1e09f73d66829bbd9a8
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-14 08:19:20 +0300

    final

--- pages/api/output.js
+++ pages/api/output.js
@@ -6,11 +6,9 @@ export default async function handler(req, res) {
   const collection = db.collection("output")
   switch (req.method) {
     case "POST":
-      const newImageString = {
-        url: req.body.get('url'),
-        lat: req.body.get('lat'),
-        lng: req.body.get('lng')
-      };
+      const newImage = req.body.get('url');
+      const newLat = req.body.get('lat');
+      const newLng = req.body.get('lng');
         let maxId = 0;
   const result = await collection.aggregate([
     { $sort: { id: -1 } },
@@ -19,7 +17,12 @@ export default async function handler(req, res) {
     maxId = result[0].id;
   } //finding the maximumID 
   const newId = maxId + 1;
-  newImageString.id = newId;
+  const newImageString = {
+    id: newId,
+    url: newImage,
+    lat: newLat,
+    lng: newLng
+  };
   const addedImage = await collection.insertOne(newImageString);
   console.log(addedImage);
   if (!addedImage) {
