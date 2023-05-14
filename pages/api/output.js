e5e8cd1156f0e175f929d38475da349cf0b8caa0
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-14 18:01:50 +0300

    sssssssss

--- pages/api/output.js
+++ pages/api/output.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const collection = db.collection("output")
   switch (req.method) {
     case "POST":
-      const newImageString = { url: req.body.url };
+      const newImageString = { url: req.body.get('url'), lat: req.body.get('lat'), lng: req.body.get('lng') };
       let maxId = 0;
       const result = await collection.aggregate([
         { $sort: { id: -1 } },
