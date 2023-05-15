f87bda37f50ebc3c5c5ea2ab0186163efde02d7f
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-15 23:19:51 +0300

    sssss

--- public/js/model/reports.js
+++ public/js/model/reports.js
@@ -84,6 +84,7 @@ function camToCards(reports) {
     <div class="card-header">
     <h2 class="card-title">Accident Alert <span>!!! ID ${reports.id}</span></h2>
   </div>
+  <p>${reports.date}</p>
                 <img src="${reports.url}" />
                 <div class="card-location">
                 <p>Location: <a href="https://www.google.com/maps/search/?api=1&query=${reports.lat},${reports.lng}" target="_blank">See location on map</a></p>
