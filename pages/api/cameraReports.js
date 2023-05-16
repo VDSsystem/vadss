9fda92d31985de202caa748d831082fe576d0991
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-16 03:34:45 +0300

    FinalCode?

--- pages/api/cameraReports.js
+++ pages/api/cameraReports.js
@@ -3,20 +3,9 @@ import modelRepo from "./repository/model-repo";
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
-  const newImageString = { url: req.body.url, lat: req.body.lat, lng: req.body.lng, dateTime: req.body.dateTime };
-  console.log(newImageString);
-  let maxId = 0;
-  const result = await collection.aggregate([
-    { $sort: { id: -1 } },
-    { $limit: 1 }]).toArray();
-  if (result.length > 0) {
-    maxId = result[0].id;
-  } //finding the maximumID 
-  const newId = maxId + 1;
-  newImageString.id = newId;
-  const addedImage = await collection.insertOne(newImageString);
-  console.log(addedImage);
-  if (!addedImage) {
+  const newImageReport = { url: req.body.url, lat: req.body.lat, lng: req.body.lng, dateTime: req.body.dateTime };
+  const response = await modelRepo.addReport(newImageReport)
+  if (!response) {
     res.status(401).json({ success: false });
   } else {
     res.status(200).json({id: newId});
--- pages/api/repository/model-repo.js
+++ pages/api/repository/model-repo.js
@@ -13,26 +13,19 @@ class ModelRepo {
         return { status: 200, data: reports };
       }
       
-    async addReport(newReport) {
-      const col = await this.connect()
-      let maxId = 0;
-      const result = await col.aggregate([
-        { $sort: { id: -1 } },
-        { $limit: 1 }]).toArray();
+    async addReport(newImageReport) {
+        const col = await this.connect()
+        let maxId = 0;
+        const result = await col.aggregate([
+          { $sort: { id: -1 } },
+          { $limit: 1 }]).toArray();
         if (result.length > 0) {
-            maxId = result[0].id;
-        }//finding the maximumID 
-        const foundReports = await col.find({}).toArray();//finding all reports
-        const foundReport = foundReports.find(r => r.espID == newReport.espID);
-        if (foundReport) {//if the report with the same espID already exists
-          newReport.id = foundReport.id;
-          newReport.letter = "-B";//give it the name B
-        } else {
-          newReport.id = maxId + 1;
-          newReport.letter = "-A";//give it the name A
-        }
-        const addedReport = await col.insertOne(newReport);
-        return addedReport;
+          maxId = result[0].id;
+        } //finding the maximumID 
+        const newId = maxId + 1;
+        newImageReport.id = newId;
+        const addedImage = await col.insertOne(newImageReport);
+        return addedImage;
     }
 }
 
